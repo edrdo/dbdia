22e244804143952737743497f99f0e797a18af14
Eduardo Marques <edrdo@dcc.fc.up.pt>
2021-01-25 12:31:10 +0000

    Post-release cleanup (again!) [skip ci]

--- src/main/java/dbdia/Main.java
+++ src/main/java/dbdia/Main.java
@@ -40,7 +40,7 @@ public enum Main {
     INSTANCE.onMain(args);
   }
 
-  static final String VERSION = "0.4";
+  static final String VERSION = "0.5-SNAPSHOT";
 
   private static final String CHARSET_TO_USE = "UTF-8";
   
